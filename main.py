550821a515d21e5244fc05baa5ba9302b9c89be6
pllopis <pllopis@faisan9.arcos.inf.uc3m.es>
2013-07-03 19:43:54 +0200

    procstat working

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+import sys, getopt
+from time import sleep
 from procstat import ProcStat
 
-st = ProcStat()
+def capture(sleeptime):
+    stat = ProcStat()
+    while True:
+        stat.update()
+        sleep(sleeptime)
+
+def main(argv):
+    sleeptime = 1
+    try:
+        opts, args = getopt.getopt(argv[1:], 't:')
+    except getopt.GetoptError:
+        print "%s -t <sleep time (s)>" % argv[0]
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-t':
+            sleeptime = arg
+    capture(sleeptime)
+
+if __name__ == '__main__':
+    main(sys.argv)
--- procstat.py
+++ procstat.py
@@ -2,17 +2,42 @@ class ProcStat:
     def __init__(self):
         self.fstat = None
         try:
-            fstat = open("/proc/stat")
+            fstat = open('/proc/stat')
         except IOError:
             raise Exception('Fatal error: no procfs') 
         else:
             self.fstat = fstat
+            self.current_data = None
+            self.diff_data = None
             self.update()
+            self.orig_data = self.current_data.copy()
+            self.last_data = self.orig_data
+            self.diff_data = self.current_data.copy() # initialize structure, will get overwritten
+
+    def difference(self):
+        if self.last_data == None:
+            self.last_data = self.current_data
+        for key in self.current_data:
+            i = 0
+            while i < len(self.current_data[key]):
+                self.diff_data[key][i] = self.current_data[key][i] - self.last_data[key][i]
+                i = i + 1
 
     def update(self):
-        self.data = self.fstat.readlines()
+        self.fstat.seek(0)
+        rawdata = self.fstat.readlines()
+        data = {}
+        for line in rawdata:
+            items = line.split()
+            data[items[0]] = map(int, items[1:])
+
+        self.last_data = self.current_data
+        self.current_data = data.copy()
+        if self.diff_data != None: # no reason to do difference when initializing class
+            self.difference()
+
 
     def __del__(self):
         # Avoid opening the file more than once
         if (self.fstat != None):
-            close(self.fstat)
+            self.fstat.close()
