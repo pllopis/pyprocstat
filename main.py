77ea04df16668f586a8e2fc3ae80abbf351bbcd8
pllopis <pllopis@faisan9.arcos.inf.uc3m.es>
2013-07-04 14:29:56 +0200

    add minimum sleep time and meminfo stats

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys, getopt
+import os, sys, getopt
 from time import sleep
 from procstat import ProcStat
 
@@ -17,7 +17,18 @@ def main(argv):
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-t':
-            sleeptime = arg
+            # sanity check for int/float supplied value
+            try:
+                sleeptime = float(arg)
+            except ValueError:
+                print "Argument for -t must be float or int. Supplied: %s" % arg
+                sys.exit(3)
+    # /proc gets updated at USER_HZ, so don't allow frequencies above this value
+    user_hz_t = float(1) / os.sysconf(os.sysconf_names['SC_CLK_TCK'])
+    if user_hz_t > sleeptime:
+        print "There is no point in having a sleep time (%s) smaller than 1/USER_HZ (%s). Supply a greater sleep time." % (sleeptime, user_hz_t)
+        sys.exit(4)
+    # capture system data at specified time intervals
     capture(sleeptime)
 
 if __name__ == '__main__':
--- procstat.py
+++ procstat.py
@@ -1,12 +1,15 @@
 class ProcStat:
     def __init__(self):
         self.fstat = None
+        self.fmem = None
         try:
             fstat = open('/proc/stat')
+            fmem = open('/proc/meminfo')
         except IOError:
             raise Exception('Fatal error: no procfs') 
         else:
             self.fstat = fstat
+            self.fmem = fmem
             self.current_data = None
             self.diff_data = None
             self.update()
@@ -24,20 +27,42 @@ class ProcStat:
                 i = i + 1
 
     def update(self):
+        self.update_stat()
+        self.update_mem()
+        
+    def update_stat(self):
         self.fstat.seek(0)
         rawdata = self.fstat.readlines()
         data = {}
         for line in rawdata:
             items = line.split()
             data[items[0]] = map(int, items[1:])
-
+        data['intr'] = [sum(data['intr'])] # summarize intr
         self.last_data = self.current_data
         self.current_data = data.copy()
         if self.diff_data != None: # no reason to do difference when initializing class
             self.difference()
+        print self.diff_data
 
+    def  update_mem(self):
+        self.fmem.seek(0)
+        rawdata = self.fmem.readlines()
+        data = {}
+        for line in rawdata:
+            if line.startswith('MemTotal'):
+                data['MemTotal'] = line.split()[1]
+            elif line.startswith('MemFree'):
+                data['MemFree'] = line.split()[1]
+            elif line.startswith('Buffers'):
+                data['Buffers'] = line.split()[1]
+            elif line.startswith('Cached'):
+                data['Cached'] = line.split()[1]
+        self.memdata = data
+        print data
 
     def __del__(self):
-        # Avoid opening the file more than once
+        # Avoid opening the file more than once. Keep file open, close on del
         if (self.fstat != None):
             self.fstat.close()
+        if (self.fmem != None):
+            self.fmem.close()
