b0c3cf4eba3aebb64800c8f758c39d73aa09f705
Pablo Llopis <pablo.llopis@gmail.com>
2014-03-19 13:11:28 +0100

    switch to docopt argument parsing

--- pyprocstat.py
+++ pyprocstat.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python
+"""
+pyprocstat. Gathers system information from /proc and streams it over the network.
+
+Usage:
+  pyprocstat.py (-h | --help)
+  pyprocstat.py [--quiet] [--sleep=<sleep_time>] [--port=<listen_port>] 
+
+Options:
+  -q --quiet      Do not print info being sent.
+  -s --sleep=<sleep time>       Sleep time between proc readings in seconds, may be floating point. [default: 1.0]
+  -p --port=<listen port>       Port to listen to. [default: 1234]
+
+"""
 
 import os, sys, getopt, socket, time, struct
 from procstat import ProcStat
+from docopt import docopt
 
 def capture(sleeptime, port, quiet=False):
     stat = ProcStat()
@@ -40,42 +54,20 @@ def capture(sleeptime, port, quiet=False):
                 s.close()
                 break
 
-def main(argv):
-    sleeptime = 1
-    port = 1234
-    quiet = False
-    try:
-        opts, args = getopt.getopt(argv[1:], 'ht:p:q')
-    except getopt.GetoptError:
-        print "%s -t <sleep time (s)>" % argv[0]
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == '-h':
-            print "%s -t <sleep time (s)> -p <listen port> -q (set quiet flag)" % argv[0]
-            sys.exit(2)
-        elif opt == '-t':
-            # sanity check for int/float supplied value
-            try:
-                sleeptime = float(arg)
-            except ValueError:
-                print "Argument for -t must be float or int. Supplied: %s" % arg
-                sys.exit(3)
-        elif opt == '-p':
-            try:
-                port = int(arg)
-            except ValueError:
-                print "Argument for -p must be an integer port number. Supplied: %s" % arg
-                sys.exit(4)
-        elif opt == '-q':
-            quiet = True
+def main(arguments):
+    sleeptime = float(arguments['--sleep'])
+    port = int(arguments['--port'])
+    quiet = arguments['--quiet']
 
     # /proc gets updated at USER_HZ, so don't allow frequencies above this value
     user_hz_t = float(1) / os.sysconf(os.sysconf_names['SC_CLK_TCK'])
     if user_hz_t > sleeptime:
         print "There is no point in having a sleep time (%s) smaller than 1/USER_HZ (%s). Supply a greater sleep time." % (sleeptime, user_hz_t)
         sys.exit(4)
+
     # capture system data at specified time intervals
     capture(sleeptime, port, quiet)
 
 if __name__ == '__main__':
-    main(sys.argv)
+    arguments = docopt(__doc__)
+    main(arguments)
