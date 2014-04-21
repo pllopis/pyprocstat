988a228ae24bcb7a7c9e674e5ab18bd26b2a9406
Pablo Llopis <pablo.llopis@gmail.com>
2014-04-21 17:41:16 +0200

    pstates

--- procstat.py
+++ procstat.py
@@ -39,6 +39,17 @@ class ProcStat:
 
         return n
 
+    def read_pstates(self, fh):
+        fh.seek(0)
+        v = fh.readlines()
+        return map(lambda x: int(x.rstrip().split()[-1]), v)
+
+    def v_diff(self, a, b):
+        '''
+        return vector a - vector b
+        '''
+        return map(lambda x: x[0] - x[1], zip(a, b))
+
     def init_cpu_data(self):
         '''
         Open files and count present cpus.
@@ -46,17 +57,23 @@ class ProcStat:
         'present' is the file handle for /sys/devices/system/cpu/present
         'num_cpus' is an integer representing the number of processors in the system
         'cstates' is a list, the ith element being the ith processor.
-        Each processor is a list of C-states, and each C-state is a dict with three keys:
-        'time_fh' is the file handle for the time in useconds spent in that C-state.
-        'usage_fh' is the file handle for the number of times that was transitioned to that C-state.
-        'latency' is the time penalty in usec for transitioning out of that C-state.
+          Each processor is a list of C-states, and each C-state is a dict with three keys:
+          'time_fh' is the file handle for the time in useconds spent in that C-state.
+          'usage_fh' is the file handle for the number of times that was transitioned to that C-state.
+          'latency' is the time penalty in usec for transitioning out of that C-state.
+        'pstates' is a list, the ith element being the ith processor.
+          Each processor is a dict with two keys:
+          'time_in_state_fh' is the file handle for /sys/devices/system/cpu/cpuN/cpufreq/stats/time_in_state
+          'time_in_state' is a list of integers for the time spent in each state, same order as time_in_state file
         '''
         data = {} 
         fpresent = open('/sys/devices/system/cpu/present')
         data['num_cpus'] = self.count_cpus(fpresent)
         fpresent.close()
         data['cstates'] = []
+        data['pstates'] = []
         for i in range(0, data['num_cpus']):
+            #cstate data
             cpu = []
             for state_path in glob.glob("/sys/devices/system/cpu/cpu%s/cpuidle/state*" % i):
                 d = {}
@@ -70,7 +87,14 @@ class ProcStat:
                 d['latency'] = self.file_read_int(join(state_path, 'latency'))
                 cpu.append(d)
             data['cstates'].append(cpu)
-       
+
+            #pstate data
+            d = {}
+            d['time_in_state_fh'] = open("/sys/devices/system/cpu/cpu%s/cpufreq/stats/time_in_state" % i)
+            d['pstates'] = None
+            d['diff'] = None
+            data['pstates'].append(d) 
+
         return data
         
     def count_cpus(self, fpresent):
@@ -85,7 +109,11 @@ class ProcStat:
         Include any summarizing/aggregation and formatting.
         While explicitly listing values to be bundled reduces
         portability across architectures, it enhances the readability
-        of what is being bundled, and in what order the data is stored.'''
+        of what is being bundled, and in what order the data is stored.
+        
+        Each line:
+        cpu[10] cpu0[10] .. cpuN[10] sum(intr) ctxt btime processes procs_running procs_blocked sum(softirq) memtotal memfree buffers cached cpu0_cstate0..cpu0_cstateN .. cpuN_cstate0..cpuN_cstateN
+        '''
         bundle = []
         # get all values from diff_data that have cpu* keys
         cpudata = dict((k, v) for k, v in self.diff_data.items() if k.startswith('cpu'))
@@ -111,12 +139,20 @@ class ProcStat:
             for cstate in cpu:
                 bundle.extend(cstate['diff'])
 
+        # bundle pstates
+        for cpu in self.cpu_data['pstates']:
+            bundle.extend(cpu['diff'])
+
         self.bundle = map(lambda x: str(x), bundle)
 
+    #TODO: use count_cpus instead of this function
     def is_single_cpu(self, key):
             return (key.startswith('cpu') and len(key) > 3) # 'cpu0', 'cpu1', etc
 
     def cstates_difference(self):
+        pass
+
+    def pstates_difference(self):
         pass
 
     def stat_difference(self):
@@ -129,22 +165,23 @@ class ProcStat:
             while i < len(self.current_data[key]):
                 self.diff_data[key][i] = self.current_data[key][i] - self.last_data[key][i]
                 i = i + 1
-        # normalize CPU aggregate
-        period = self.diff_data['totaltime'][0]
-        time = period if period != 0 else 1 # first interval period is 0
-        f = lambda x: min(1.0, float(x) / time)
-        self.diff_data['cpu'] = map(f, self.diff_data['cpu'])
-        # normalize single CPU data
-        for key in self.diff_data:
-            if self.is_single_cpu(key):
-                old = self.diff_data[key]
-                self.diff_data[key] = map(f, self.diff_data[key])
-        #        print "(%s %s => %s)" % (key, old, self.diff_data[key])
+        ## normalize CPU aggregate
+        #period = self.diff_data['totaltime'][0]
+        #time = period if period != 0 else 1 # first interval period is 0
+        #f = lambda x: min(1.0, float(x) / time)
+        #self.diff_data['cpu'] = map(f, self.diff_data['cpu'])
+        ## normalize single CPU data
+        #for key in self.diff_data:
+        #    if self.is_single_cpu(key):
+        #        old = self.diff_data[key]
+        #        self.diff_data[key] = map(f, self.diff_data[key])
+        ##        print "(%s %s => %s)" % (key, old, self.diff_data[key])
 
     def diff(self):
         if self.diff_data != None: # no reason to do difference when initializing class
             self.stat_difference()
             self.cstates_difference()
+            self.pstates_difference()
             self.create_bundle()
         #print "(diff_data %s)" % self.diff_data
 
@@ -152,6 +189,7 @@ class ProcStat:
         self.update_stat()
         self.update_mem()
         self.update_cstates()
+        self.update_pstates()
         self.diff()
 
     def update_cstates(self):
@@ -168,6 +206,14 @@ class ProcStat:
                                     cstate['usage'] - cstate['usage_last'] ]
                 #print "%s - %s = %s" % (cstate['time'], cstate['time_last'], cstate['diff'])
 
+    def update_pstates(self):
+        for cpu in self.cpu_data['pstates']:
+            cpu['pstates_last'] = cpu['pstates']
+            cpu['pstates'] = self.read_pstates(cpu['time_in_state_fh'])
+            if cpu['pstates_last'] == None:
+                cpu['pstates_last'] = cpu['pstates']
+            cpu['diff'] = self.v_diff(cpu['pstates'], cpu['pstates_last'])
+
     def update_stat(self):
         self.fstat.seek(0)
         rawdata = self.fstat.readlines()
@@ -182,7 +228,8 @@ class ProcStat:
             if line.startswith('cpu') and (not line.startswith('cpu ')):
                 num_cpus = num_cpus + 1
         # totaltime = user + nice + system + idle + steal + guest
-        totaltime = (data['cpu'][0] + data['cpu'][1] + data['cpu'][2] + data['cpu'][3] + data['cpu'][7] + data['cpu'][8]) / num_cpus
+#        totaltime = (data['cpu'][0] + data['cpu'][1] + data['cpu'][2] + data['cpu'][3] + data['cpu'][7] + data['cpu'][8]) / num_cpus
+        totaltime = sum(data['cpu']) / num_cpus
         data['totaltime'] = [ totaltime ] 
         self.last_data = self.current_data
         self.current_data = data.copy()
@@ -214,6 +261,9 @@ class ProcStat:
                 for cstate in cpu:
                     cstate['time_fh'].close()
                     cstate['usage_fh'].close()
+            for cpu in self.cpu_data['pstates']:
+                cpu['time_in_state_fh'].close()
+                
 
     def __str__(self):
         return "%s\n" % ' '.join(self.bundle)
